99b648acbf599aec700bd2ca486c00813b781c1f
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2022-06-06 21:24:22 -0300

    Criação do servidor com express

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log('Olá Mundo');
+const app = require("./src/app");
+const PORT = 3000;
+// 3000, 3030, 3333, 6000, 6060, 8000, 8080
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}.`)
+})
