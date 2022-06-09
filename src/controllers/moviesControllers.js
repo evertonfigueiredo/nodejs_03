4f5a73e042bcedbf9a236dc412a68707a00477de
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2022-06-08 21:57:57 -0300

    delete de filmes e postman

--- src/controllers/moviesControllers.js
+++ src/controllers/moviesControllers.js
@@ -23,14 +23,63 @@ const getAllGhibliMovies = async (req, res) => {
 
     const response = await axios.get(url);
     const data = response.data;
+
     res.status(200).send(data);
   } catch (e) {
       console.error(e);
   }
 };
 
+// metodo para criar um novo filme
+
+// cadastrar novo filme
+const createMovie = (req, res) => {
+    // acessar os dados enviados na requisição
+    let requestedTitle = req.body.title;
+    let requestedGenre = req.body.genre;
+
+    if (requestedTitle && requestedGenre) {
+        let newMovie = {
+            "id": Math.random().toString(32).substr(2, 6),
+            "addedAt": new Date(),
+            "title": requestedTitle,
+            "genre": requestedGenre
+        };
+
+        movies.push(newMovie);
+
+        // enviar uma resposta
+        res.status(201).send({
+            "mensagem": "Filme adicionado com sucesso",
+            newMovie
+        });
+    } else {
+        res.status(404).send({
+            "message": "Não foi possível cadastrar um novo filme. Por favor, insira todas as informações necessárias"
+        })
+    }
+};
+
+
+//  fazer metodo para deletar um filme
+const deleteMovieById = (req, res) => {
+    const requestedId = req.params.id;
+    const filteredMovie = movies.find(movie => movie.id == requestedId);
+
+    const index = movies.indexOf(filteredMovie);
+
+    movies.splice(index, 1);
+
+    res.status(200).json([{
+        "mensagem": "Filme deletado com sucesso",
+        movies
+    }]);
+};
+
 module.exports = {
   home,
   getAll,
   getAllGhibliMovies,
+  createMovie,
+  deleteMovieById
 };
--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -6,9 +6,11 @@ const controller = require("../controllers/moviesControllers");
 // console.log("INFORMAÇÕES TRAZIDAS NO CONTROLLER", controller);
 
 // Router.verboHTTP(ROTA, CONTROLLER)
-router.get('/', controller.home)
-router.get('/all', controller.getAll)
-router.get('/ghibli', controller.getAllGhibliMovies)
+router.get("/", controller.home)
+router.get("/all", controller.getAll)
+router.get("/ghibli", controller.getAllGhibliMovies)
+router.post("/create/movie", controller.createMovie);
+router.delete("/delete/movie/:id", controller.deleteMovieById);
 
 // router.post()
 // router.delete()
